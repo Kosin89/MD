74ffe10d0a3006bac570fbc0e54998094377fcee
Kosin89 <110737422+Kosin89@users.noreply.github.com>
2022-10-14 22:15:16 -0500

    added contact and footer

--- js/main.js
+++ js/main.js
@@ -12,26 +12,26 @@ document.querySelectorAll(".nav__link").forEach(n => n.addEventListener("click",
     navMenu.classList.remove("active");
 }))
 
-let section = document.querySelector(".container");
-let stat = document.querySelectorAll(".stats .sta");
-let start = false;
+// let section = document.querySelector(".container");
+// let stat = document.querySelectorAll(".stats .sta");
+// let start = false;
 
 
-window.onscroll = function(){
-    if(window.scrollY >= section.offsetTop){
-        if(!start){
-            stat.forEach((sta) => startCount(sta));
-        }
-        start = true;
-    }
-}
+// window.onscroll = function(){
+//     if(window.scrollY >= section.offsetTop){
+//         if(!start){
+//             stat.forEach((sta) => startCount(sta));
+//         }
+//         start = true;
+//     }
+// }
 
-function startCount(el){
-    let max = el.dataset.max;
-    let count = setInterval(() => {
-        el.textContent++;
-        if(el.textContent == max) {
-            clearInterval(count);
-        }
-    }, 500 / stat);
-}
+// function startCount(el){
+//     let max = el.dataset.max;
+//     let count = setInterval(() => {
+//         el.textContent++;
+//         if(el.textContent == max) {
+//             clearInterval(count);
+//         }
+//     }, 500 / stat);
+// }
